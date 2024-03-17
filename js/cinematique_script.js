2bde6bca9dc4a4075433174f41da87a232b091b8
Daniel <daniel.ferriere41@gmail.com>
2024-03-17 22:02:32 +0100

    create a test page for kinetic training

---
+++ js/cinematique_script.js
@@ -0,0 +1,33 @@
+import {body, sidebar, toggle, search, modeSwitch, modeText} from "./script.js"
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("gameBtn1");
+    
+    trainingBtn.addEventListener("click", function(event) {
+        1/0;
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("gameBtn2");
+    
+    trainingBtn.addEventListener("click", function(event) {
+        1/0;
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("gameBtn3");
+    
+    trainingBtn.addEventListener("click", function(event) {
+        1/0;
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("gameBtn4");
+    
+    trainingBtn.addEventListener("click", function(event) {
+        1/0;
+    })
+});
