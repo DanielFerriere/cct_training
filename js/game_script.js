fed0d306e83266070c7a3ed02defcfb67d4c806c
Daniel <daniel.ferriere41@gmail.com>
2024-04-23 20:04:44 +0200

    change name of the previous first game class and modify file link to it

--- js/game_script.js
+++ js/game_script.js
@@ -24,7 +24,9 @@ class Game {
     reply() {}
 }
 
-class GameImg2C extends Game{
+
+
+class GameImg2Choice extends Game {
 
     constructor(imagesDir, gameImg, button1, button2, button3, button4, modal, modalContent, retryBtn) {
         super();
@@ -62,7 +64,6 @@ class GameImg2C extends Game{
             self.begin();
         });
     }
-
 
 
     async begin() {
@@ -141,4 +142,100 @@ class GameImg2C extends Game{
 }
 
 
-export {GameImg2C};
+
+class GameImg2Word extends Game {
+
+    constructor(imagesDir, gameImg, gameForm, gameInput, sendBtn, modal, modalContent, retryBtn) {
+        super();
+
+        this.imagesDir = imagesDir;
+
+        this.gameImg = gameImg;
+        this.gameForm = gameForm;
+        this.gameInput = gameInput;
+        this.sendBtn = sendBtn;
+
+        this.modal = modal;
+        this.modalContent = modalContent;
+        this.retryBtn = retryBtn;
+
+        var self = this;
+        gameForm.addEventListener("submit", function(event) {
+            event.preventDefault();
+            console.log("Response get submit");
+            self.reply();
+        });
+        
+        retryBtn.addEventListener("click", function(event) {
+            console.log("Retry button has been click");
+            self.begin();
+        });
+    }
+
+    async begin() {
+        const response = await fetch(this.imagesDir + "/image.json");
+        var json_data = await response.json();
+        this.references = json_data["name_file_fr"];
+
+        this.score = 0;
+        this.score_max = this.references.length;
+
+        this.guessing = this.references.slice();
+        utils.shuffle(this.guessing);
+
+        this.modal.classList.remove("show-modal");
+        this.modalContent.classList.remove("show-modal-content");
+        this.new_guess();
+    }
+
+    finish_game() {
+        for (let i =0; i<4; i++) this.buttons[i].disabled = true;
+
+        let textModal = this.modalContent.querySelector("h2");
+        textModal.innerText = "Votre score est de ".concat(this.score,"/",this.score_max);
+        this.modalContent.classList.toggle("show-modal-content");
+
+        this.modal.classList.toggle("show-modal");
+    }
+
+    new_guess() {
+        //get the new guess
+        let guess = this.guessing.shift();
+        this.answer = guess["name"];
+
+        //change the game image
+        this.gameImg.src = this.imagesDir + "/".concat(guess["file"]);
+
+        //resore input state
+        this.gameInput.value = "";
+        this.gameInput.disabled = false;
+        this.sendBtn.disabled = false;
+        this.gameInput.classList.remove("right");
+        this.gameInput.classList.remove("wrong");
+    }
+
+    reply() {
+        this.gameInput.disabled = true;
+        this.sendBtn.disabled = true;
+
+        if (this.gameInput.value.toLowerCase() == this.answer.toLowerCase()) {
+            this.add_point();
+            this.gameInput.classList.toggle("right");
+        } else {
+            this.gameInput.classList.toggle("wrong");
+        }
+
+        if (this.guessing.length == 0) {
+            this.finish_game();
+            return;
+        }
+
+        setTimeout(() => {
+            this.new_guess();
+        }, 2000);
+    }
+}
+
+
+
+export {GameImg2Choice, GameImg2Word};
