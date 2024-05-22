5ff02704f94393e2ed2b2adacf85b929e1f68a11
Daniel <daniel.ferriere41@gmail.com>
2024-05-22 22:52:15 +0200

    i dont know what i do

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const body = document.querySelector("body"),
+      sidebar = document.querySelector(".sidebar"),
+      toggle = document.querySelector(".toggle"),
+      search = document.querySelector(".search-box"),
+      modeSwitch = document.querySelector(".toggle-switch"),
+      modeText = document.querySelector(".mode-text");
+
+
+      
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+search.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light Mode"
+    }else{
+        modeText.innerText = "Dark Mode"
+    }
+})
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dashboardBtn = document.getElementById("dashboardBtn");
+    
+    dashboardBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "./index.html";
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const trainingBtn = document.getElementById("trainingBtn");
+    
+    trainingBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "./training.html";
+    })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const logoutBtn = document.getElementById("logoutBtn");
+    
+    logoutBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        // Effacer les informations d'identification de l'utilisateur stockées localement
+        localStorage.removeItem("currentUser");
+
+        // Rediriger vers la page de connexion
+        window.location.href = "./login.html";
+    });
+});
+
+
