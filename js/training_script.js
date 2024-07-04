72b7baa252a97c42700ab6568b670108ddeeeb30
Daniel <daniel.ferriere41@gmail.com>
2024-07-05 00:24:22 +0200

    try puting graph into the placeholder in training page

--- js/training_script.js
+++ js/training_script.js
@@ -48,7 +48,6 @@ materialBtn.addEventListener("click", function() {
 
 
 
-
 arrayGamemodeBtn[0][0].addEventListener("click", function(event) {
     event.preventDefault();
     window.location.href = "./page/shape_game1.html";
@@ -123,11 +122,6 @@ arrayGamemodeBtn[2][3].addEventListener("click", function(event) {
 });
 
 
-
-
-
-
-
 for (let i=0; i<arrayBackBtn.length; i++) {
     arrayBackBtn[i].addEventListener("click", function () {
         hide_modal();
@@ -138,3 +132,53 @@ for (let i=0; i<arrayBackBtn.length; i++) {
 
 
 
+// set the dimensions and margins of the graph
+const margin = {top: 30, right: 30, bottom: 30, left: 30};
+// append the svg object to the body of the page
+const svg = d3.select("#graphDiv")
+  .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+const svghtml = svg.node();
+
+//Read the data
+d3.json("../test_data.json").then(
+  // Now I can use this dataset:
+  function(data) {
+    console.log(data);
+    // Add X axis --> it is a date format
+    const x = d3.scalePoint()
+      .range([ 0, svghtml.clientWidth])
+      .domain([1,2,3,"D",5,6,7])
+    svg.append("g")
+      .attr("transform", "translate(0," + svghtml.clientHeight + ")")
+      .call(d3.axisBottom(x));
+    // Add Y axis
+    const y = d3.scaleLinear()
+      .domain( [0, 1.0])
+      .range([ svghtml.clientHeight, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "#69b3a2")
+      .attr("stroke-width", 4)
+      .attr("d", d3.line()
+        .x(d => x(d.occ))
+        .y(d => y(d.value))
+        )
+    // Add the points
+    svg.append("g")
+        .selectAll("dot")
+        .data(data)
+        .join("circle")
+        .attr("cx", d => x(d.occ))
+        .attr("cy", d => y(d.value))
+        .attr("r", 8)
+        .attr("fill", "#69b3a2")
+})
